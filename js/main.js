6cc0807a3fafa41cdbc454c4860d610642829bd6
masanori <saitoumasanori17@gmail.com>
2020-01-17 16:35:40 +0900

    ハンバーガーメニュー追加

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const hamOpen = document.getElementById("ham_open")
+
+hamOpen.addEventListener("click", function () {
+    document.getElementById(`ham_menu`)
+    .classList.toggle("open")
+})
+
+hamOpen.addEventListener("click", function () {
+    document.getElementsByClassName(`ham_top`)[0].classList.toggle("open")
+    document.getElementsByClassName(`ham_middle`)[0].classList.toggle("open")
+    document.getElementsByClassName(`ham_bottom`)[0].classList.toggle("open")
+})
