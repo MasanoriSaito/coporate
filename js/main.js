e976476c40357edb36ff14cf1d3276fbe34e8427
masanori <saitoumasanori17@gmail.com>
2020-01-17 16:54:18 +0900

    ハンバーガーメニュースマホ変更

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ const hamOpen = document.getElementById("ham_open")
 hamOpen.addEventListener("click", function () {
     document.getElementById(`ham_menu`)
     .classList.toggle("open")
+    
 })
 
 hamOpen.addEventListener("click", function () {
     document.getElementsByClassName(`ham_top`)[0].classList.toggle("open")
     document.getElementsByClassName(`ham_middle`)[0].classList.toggle("open")
     document.getElementsByClassName(`ham_bottom`)[0].classList.toggle("open")
+})
+
+hamOpen.addEventListener("click", function () {
+    document.getElementsByClassName(`fv_wrap__catch`)[0].classList.toggle("open")
 })
